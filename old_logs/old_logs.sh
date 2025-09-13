7d28fc5bc5a9e50cef1017f0bbdee7b8468908c0
AjayKumar-12-06 <meajaykumar.p97@gmail.com>
2025-09-13 22:58:09 +0530

    script for the delattion of old loga

--- old_logs/old_logs.sh
+++ old_logs/old_logs.sh
@@ -2,6 +2,6 @@
 
 source_dir="/home/ec2-user"
 
-find_old_log=$( find $source_dir -name "*.log" -mtime 14)
+find_old_log=$( find $source_dir -name "*.log" -mtime +14)
 
 echo "The old logs are:" $find_old_log
